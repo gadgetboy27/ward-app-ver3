52ba21a672915f2dce6543ac3f7be4063afe2462
Henry-Peti <iamgadgetboy@gmail.com>
2019-10-27 17:50:36 +1300

    ward app working add & remove patients

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-// Book class: Represents a book
-class Book {
-  constructor (title, author, isbn) {
-    this.title = title
-    this.author = author
-    this.isbn = isbn
+// patient class: Represents a book
+class Patient {
+  constructor (name, room, gest, notes) {
+    this.name = name
+    this.room = room
+    this.gest = gest
+    this.notes = notes
   }
 }
-// Ui class: Handle UI Tasks
+// UI class: Handle UI Tasks
 class UI {
-  static displayBooks () {
-    const books = Store.getBooks()
+  static displayPatients () {
+    const patients = Store.getPatients()
 
-    books.forEach((book) => UI.addBookToList(book))
+    patients.forEach((patient) => UI.addpatientToList(patient))
   }
-  static addBookToList (book) {
-    const list = document.querySelector('#book-list')
+  static addpatientToList (patient) {
+    const list = document.querySelector('#patient-list')
 
     const row = document.createElement('tr')
 
     row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class='btn btn-danger btn-sml 
-        delete'>X</td>
+        <td>${patient.name}</td>
+        <td>${patient.room}</td>
+        <td>${patient.gest}</td>
+        <td>${patient.notes}</td>
+        <td><a href="#" class='btn btn-sml 
+        update'>✅</td>
+        <td><a href="#" class='btn btn-med 
+        delete'>❌</td>
         `
     list.appendChild(row)
   }
 
-  static deleteBook (el) {
+  static deletepatient (el) {
     if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove()
     }
@@ -39,7 +43,7 @@ class UI {
     div.className = `alert alert-${className}`
     div.appendChild(document.createTextNode(message))
     const container = document.querySelector('.container')
-    const form = document.querySelector('#book-form')
+    const form = document.querySelector('#patient-form')
     container.insertBefore(div, form)
 
     // Alert Disappers in 3 secs
@@ -47,78 +51,84 @@ class UI {
   }
 
   static clearFields () {
-    document.querySelector('#title').value = ''
-    document.querySelector('#author').value = ''
-    document.querySelector('#isbn').value = ''
+    document.querySelector('#name').value = ''
+    document.querySelector('#room').value = ''
+    document.querySelector('#gest').value = ''
+    document.querySelector('#notes').value = ''
   }
 }
 // Store Class: Handles Storage
 class Store {
-  static getBooks () {
-    let books
-    if (localStorage.getItem('books') === null) {
-      books = []
+  static getpatients () {
+    let patients
+    if (localStorage.getItem('patients') === null) {
+      patients = []
     } else {
-      books = JSON.parse(localStorage.getItem('books'))
+      patients = JSON.parse(localStorage.getItem('patients'))
     }
-    return books
+    return patients
   }
 
-  static addBook (book) {
-    const books = Store.getBooks()
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
+  static addpatient (patient) {
+    const patients = Store.getpatients()
+    patients.push(patient)
+    localStorage.setItem('patients', JSON.stringify(patients))
   }
 
-  static removeBook (isbn) {
-    const books = Store.getBooks()
-    books.forEach((book, index) => {
-      if (book.isbn === isbn) {
-        books.splice(index, 1)
+  static getPatientDetails (patients) {
+    const getDetails = Store.getpatients()
+    localStorage.getItem('patients', JSON.parse(getDetails))
+  }
+
+  static removepatient (room) {
+    const patients = Store.getpatients()
+    patients.forEach((patient, room) => {
+      if (patient.room === room) {
+        patients.splice(room, 1)
       }
     })
-    localStorage.setItem('books', JSON.stringify(books))
+    localStorage.setItem('patients', JSON.stringify(patients))
   }
 }
-// Event: Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
+// Event: Display patients
+document.addEventListener('DOMContentLoaded', UI.displaypatients)
 
-// Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+// Event: Add a patient
+document.querySelector('#patient-form').addEventListener('submit', (e) => {
   e.preventDefault()
-  // Get the values of new book to Add to list
-  const title = document.querySelector('#title').value
-  const author = document.querySelector('#author').value
-  const isbn = document.querySelector('#isbn').value
-
+  // Get the values of new patient to Add to list
+  const name = document.querySelector('#name').value
+  const room = document.querySelector('#room').value
+  const gest = document.querySelector('#gest').value
+  const notes = document.querySelector('#notes').value
   // Validate all fields
-  if (title === '' || author === '' || isbn === '') {
-    // Insert book alert
+  if (name === '' || room === '' || gest === '') {
+    // Insert patient alert
     UI.showAlert('You havn\'t filled in all fields', 'danger')
   } else {
-    // Instatiate book
-    const book = new Book(title, author, isbn)
+    // Instatiate patient
+    const patient = new Patient(name, room, gest, notes)
 
-    // Adds new book to UI
-    UI.addBookToList(book)
+    // Adds new patient to UI
+    UI.addpatientToList(patient)
 
-    // Add book to storage
-    Store.addBook(book)
+    // Add patient to storage
+    Store.addpatient(patient)
 
     // Show success message
-    UI.showAlert('book added', 'success')
+    UI.showAlert('patient added', 'success')
     // Clear field values after submitting
     UI.clearFields()
   }
 })
 
-// Event: Remove a book
-document.querySelector('#book-list').addEventListener('click', (e) => {
-  // Remove book from list
-  UI.deleteBook(e.target)
+// Event: Remove a patient
+document.querySelector('#patient-list').addEventListener('click', (e) => {
+  // Remove patient from list
+  UI.deletepatient(e.target)
 
-  // Remove book from store
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
-  // Show alert book deleted
-  UI.showAlert('Book deleted', 'success')
+  // Remove patient from store
+  Store.removepatient(e.target.parentElement.previousElementSibling.textContent)
+  // Show alert patient deleted
+  UI.showAlert('patient deleted', 'success')
 })
